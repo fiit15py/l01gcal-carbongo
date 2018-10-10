cbde27c0af2c2fe2f201d43f28830cc81190cb22
Georgius <carbongo@hotmail.com>
2018-10-10 16:50:42 +0900

    Pre-final

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ def main():
     curr_sheet = book.sheets()[8]
 
     _curr_day = ['MO','TU','WE','TH','FR','SA']
+    day_counter = -1
     _time_start = ['08:00', '09:50', '11:40', '14:00', '15:45', '17:30']
     _time_end = ['09:35', '11:25', '13:15', '15:35', '17:20', '19:05']
 
     for i in range(3,39): 
+        if (i-3)%6==0:
+            day_counter = day_counter+1
         if curr_sheet.cell(i,8).value== "":
             continue
         _name = curr_sheet.cell(i, 8).value
@@ -47,7 +50,7 @@ def main():
                 'timeZone': 'Asia/Yakutsk',
             },
             'recurrence': [
-                'RRULE:FREQ=WEEKLY;BYDAY=' + _curr_day[(i-3)%6%6]
+                'RRULE:FREQ=WEEKLY;BYDAY=' + _curr_day[day_counter]
             ]
         }
         recurring_event = service.events().insert(calendarId='primary', body=event).execute()
